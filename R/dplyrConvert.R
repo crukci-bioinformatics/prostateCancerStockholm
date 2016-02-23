d040d00afb36933082c139bc5cc7682cb0dfd856
markdunning <mark.dunning@gmail.com>
2016-02-23 12:29:46 +0000

    speed-up the convert function hopefullt

--- R/dplyrConvert.R
+++ R/dplyrConvert.R
@@ -11,11 +11,11 @@ dplyrConvert <- function(){
   
   iqrs <- apply(log2(exprs(geoData)), 1, IQR,na.rm=TRUE)
   
-  mu <-   apply(log2(exprs(geoData)), 1, mean,na.rm=TRUE)
-  sd <- apply(log2(exprs(geoData)), 1, sd,na.rm=TRUE)
+  mu <-   rowMeans(exprs(geoData))
+  sd <- genefilter:::rowSds(exprs(geoData))
   
-  camcap <- tbl_df(data.frame(Probe=featureNames(geoData),IQR=iqrs,mu,sd,log2(exprs(geoData)))) %>%
-    gather(geo_accession,Expression,- c(Probe,IQR))
+  camcap <- tbl_df(data.frame(Probe=featureNames(geoData),IQR=iqrs,mu,sd,exprs(geoData))) %>%
+    gather(geo_accession,Expression,- c(Probe,IQR,mu,sd))
   
   fd <- tbl_df(fData(geoData)) %>% rename(Probe = ID,Gene=Symbol) %>% 
     select(Probe, Gene, Entrez_Gene_ID)
