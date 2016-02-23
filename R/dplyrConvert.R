c99b867fcdef17479356b744a1076fade5dbf390
markdunning <mark.dunning@gmail.com>
2016-02-23 10:41:18 +0000

    new convert script

--- R/dplyrConvert.R
+++ R/dplyrConvert.R
@@ -5,13 +5,16 @@ dplyrConvert <- function(){
   library(tidyr)
   library(dplyr)
   library(Biobase)
-  geoData <- camcap
+  geoData <- stockholm
   
   zscore <- function(x) t(scale(t(x)))
   
   iqrs <- apply(log2(exprs(geoData)), 1, IQR,na.rm=TRUE)
   
-  camcap <- tbl_df(data.frame(Probe=featureNames(geoData),IQR=iqrs,zscore(exprs(geoData)))) %>%
+  mu <-   apply(log2(exprs(geoData)), 1, mean,na.rm=TRUE)
+  sd <- apply(log2(exprs(geoData)), 1, sd,na.rm=TRUE)
+  
+  camcap <- tbl_df(data.frame(Probe=featureNames(geoData),IQR=iqrs,mu,sd,log2(exprs(geoData)))) %>%
     gather(geo_accession,Expression,- c(Probe,IQR))
   
   fd <- tbl_df(fData(geoData)) %>% rename(Probe = ID,Gene=Symbol) %>% 
@@ -19,14 +22,9 @@ dplyrConvert <- function(){
   
   pd <- tbl_df(pData(geoData))
   
-
+  
   camcap <- camcap %>% full_join(pd) %>% full_join(fd)
 
   ##selecting most variable probe for each gene
 
-    varProbes <- camcap %>% group_by(Gene) %>% 
-    summarise(Probe = Probe[which.max(IQR)])
-  
-  camcap <- inner_join(camcap, varProbes,by="Probe") %>% rename(Gene = Gene.x) %>% select(-c(Gene.y))
-  camcap
 }
